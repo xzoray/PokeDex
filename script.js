6ecfdeea84868d1e0d051c7264211a18a447586d
Sohrab Arenja <sohrab-arenja8@hotmail.de>
2025-02-08 21:25:35 +0100

    bg color matches the type

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const pokemonArr = [];
 async function fetchPokeData() {
     const pokemonContainer = document.getElementById("content");
     pokemonContainer.innerHTML = "";
-    for (let i = 1; i <= 30; i++) {
+    for (let i = 1; i <= 151; i++) {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
       const pokemonData = await response.json();
       pokemonArr.push(pokemonData);
@@ -14,12 +14,14 @@ async function fetchPokeData() {
 }
 
 function pokeCardTemplate(pokemon) {
-    const types = pokemon.types.map(typeInfo => typeInfo.type.name).join(" ");
-    
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+    const typeClass = types.length > 0 ? `type-${types[0]}` : '';
+
     return `
-      <div class="poke-card">
+      <div class="poke-card ${typeClass}">
         <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}">
         <h3>${pokemon.name.toUpperCase()}</h3>
-        <p class="types">${types}</p>
-      </div>`;
+        <p class="types">${types.join(' ')}</p>
+      </div>
+    `;
 }
