b33acd4394cc4d05e4b923cd2a46e67a8bd496ec
Sohrab Arenja <sohrab-arenja8@hotmail.de>
2025-02-08 20:14:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const pokemonArr = [];
+
+async function fetchPokeData() {
+    const pokemonContainer = document.getElementById("content");
+    pokemonContainer.innerHTML = "";
+    for (let i = 1; i <= 30; i++) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const pokemonData = await response.json();
+      pokemonArr.push(pokemonData);
+    }
+    pokemonArr.forEach(pokemon => {
+        pokemonContainer.innerHTML += pokeCardTemplate(pokemon);
+    })
+}
+
+function pokeCardTemplate(pokemon) {
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name).join(" ");
+    
+    return `
+      <div class="poke-card">
+        <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}">
+        <h3>${pokemon.name.toUpperCase()}</h3>
+        <p class="types">${types}</p>
+      </div>`;
+}
